e3769b99f89c20e760086f1b7aed1a8b7157dc0a
Jondal32 <52935566+Jondal32@users.noreply.github.com>
2021-02-26 11:22:01 +0100

    Add files via upload

---
+++ web_cam_detection_keras.py
@@ -0,0 +1,187 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+import ctypes  # An included library with Python install.
+
+
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+                                 (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            # add the face and bounding boxes to their respective
+            # lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return (locs, preds)
+
+
+
+
+# load our serialized face detector model from disk
+print("[INFO] loading face detector model...")
+prototxtPath = 'models/face_detector/deploy.prototxt'
+weightsPath =  'models/face_detector/res10_300x300_ssd_iter_140000.caffemodel'
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+print("[INFO] loading face mask detector model...")
+maskNet = load_model('models/face_detection_mobilenetv2')
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0,framerate=30).start()
+
+#video = cv2.VideoCapture("videos/20201214_145111.mp4")
+
+#video.set(cv2.CAP_PROP_FPS, 60.0)
+
+
+
+time.sleep(2.0)
+
+no_face_mask_detected_counter = 0
+mask_counter = 0
+width = 500
+alert = False
+totalFrames = 0
+
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    #ret, frame = video.read()
+    #frame = cv2.flip(frame, flipCode=-1)
+
+    #frame = imutils.resize(frame, 300)
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=width)
+    totalFrames += 1
+
+    # detect faces in the frame and determine if they are wearing a
+    # face mask or not
+    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+    # loop over the detected face locations and their corresponding
+    # locations
+    for (box, pred) in zip(locs, preds):
+        # unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+
+        # determine the class label and color we'll use to draw
+        # the bounding box and text
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+        # display the label and bounding box rectangle on the output
+        # frame
+        cv2.putText(frame, label, (startX, startY - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+        if mask or withoutMask > 0.5:
+
+            if mask < withoutMask:
+                no_face_mask_detected_counter += 1
+
+            if mask > withoutMask:
+                mask_counter += 1
+                if mask_counter % 10 == 0:
+                    no_face_mask_detected_counter = 0
+                    alert = False
+
+            if no_face_mask_detected_counter > 25:
+                cv2.putText(frame, "Achtung bitte eine Maske tragen", (10, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                alert = True
+                no_face_mask_detected_counter -= 2
+        else:
+            alert = False
+
+
+
+
+
+    # show the output frame
+    if alert and totalFrames % 2 == 0:
+        frame[:, :, 0] = 50
+        frame[:, :, 1] = 50
+        cv2.imshow("Frame", frame)
+
+    else:
+        cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
+
+
+##ctypes.windll.user32.MessageBoxW(0, "Your text", "Your title", 1)
